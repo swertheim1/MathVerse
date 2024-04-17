ee8a9a4cef96bba609b3ac75ebe9f826614d5e2d
swertheim1 <115668509+swertheim1@users.noreply.github.com>
2024-04-16 23:56:49 -0500

    trying to fix deployment issue

--- backend/services/numbersetService.js
+++ backend/services/numbersetService.js
@@ -1,39 +1,39 @@
-const logger = require("../winston");
-const pool = require("../pool");
+// const logger = require("../winston");
+// const pool = require("../pool");
 
 
-async function fetchNumberSets(grade_level) {
-    logger.info('FetchingNumberSets function in numberSetService called');
-    logger.debug(grade_level);
+// async function fetchNumberSets(grade_level) {
+//     logger.info('FetchingNumberSets function in numberSetService called');
+//     logger.debug(grade_level);
 
-    // Validate input parameters
-    if (!grade_level) {
-        logger.warn("Grade level parameter is missing")
-        throw new Error('Grade level parameter is required');
-    }
+//     // Validate input parameters
+//     if (!grade_level) {
+//         logger.warn("Grade level parameter is missing")
+//         throw new Error('Grade level parameter is required');
+//     }
 
-    // Fetch numbersets based on grade_level from the database
-    const numberSets = await pool.query("SELECT * FROM numbersets WHERE grade_level = ?", [grade_level]);
-    const rows = numberSets[0];      
+//     // Fetch numbersets based on grade_level from the database
+//     const numberSets = await pool.query("SELECT * FROM numbersets WHERE grade_level = ?", [grade_level]);
+//     const rows = numberSets[0];      
 
-    // Check if any rows are returned
-    if (rows.length === 0) {
-        throw new Error('No numberSets found for the given grade level');
-    }
-    logger.log('numberSets', numberSets);
+//     // Check if any rows are returned
+//     if (rows.length === 0) {
+//         throw new Error('No numberSets found for the given grade level');
+//     }
+//     logger.log('numberSets', numberSets);
 
-    // Extract relevant data from the rows
-    const numberSetsData = rows.map(row => ({
-        numberset_name: row.numberset_name,
-    }));
+//     // Extract relevant data from the rows
+//     const numberSetsData = rows.map(row => ({
+//         numberset_name: row.numberset_name,
+//     }));
 
-    // Log each numberset name
-    numberSetsData.forEach(numberSet => {
-        logger.info(numberSet.numberset_name);
-    });
+//     // Log each numberset name
+//     numberSetsData.forEach(numberSet => {
+//         logger.info(numberSet.numberset_name);
+//     });
 
-    console.log('numberSetsData', numberSetsData);
-    return numberSetsData;
-}
+//     console.log('numberSetsData', numberSetsData);
+//     return numberSetsData;
+// }
 
-module.exports = fetchNumberSets;
+// module.exports = fetchNumberSets;
--- backend/services/topicService.js
+++ backend/services/topicService.js
@@ -1,43 +1,43 @@
-const logger = require("../winston");
-const pool = require("../pool");
+// const logger = require("../winston");
+// const pool = require("../pool");
 
 
-async function fetchTopics(grade_level) {
-    console.log('FetchingTopics function in topicsService called');
-    try {
-        // Validate input parameters
-        if (!grade_level) {
-            logger.debug(`Grade level parameter: ${grade_level}`);
-            throw new Error('Grade level parameter is required');
-        }
+// async function fetchTopics(grade_level) {
+//     console.log('FetchingTopics function in topicsService called');
+//     try {
+//         // Validate input parameters
+//         if (!grade_level) {
+//             logger.debug(`Grade level parameter: ${grade_level}`);
+//             throw new Error('Grade level parameter is required');
+//         }
 
-        // Fetch topics based on grade_level from the database
-        const topics = await pool.query("SELECT * FROM topics WHERE grade_level = ?", [grade_level]);
-        logger.info(`Fetched topics: ${JSON.stringify(topics[0])}`);
-        const rows = topics[0];        
+//         // Fetch topics based on grade_level from the database
+//         const topics = await pool.query("SELECT * FROM topics WHERE grade_level = ?", [grade_level]);
+//         logger.info(`Fetched topics: ${JSON.stringify(topics[0])}`);
+//         const rows = topics[0];        
 
-        // Check if any rows are returned
-        if (rows.length === 0) {
-            throw new Error('No topics found for the given grade level');
-        }
+//         // Check if any rows are returned
+//         if (rows.length === 0) {
+//             throw new Error('No topics found for the given grade level');
+//         }
 
-        // Extract relevant data from the rows
-        const topicsData = rows.map(row => ({
-            topic_name: row.topic_name,
-        }));
+//         // Extract relevant data from the rows
+//         const topicsData = rows.map(row => ({
+//             topic_name: row.topic_name,
+//         }));
 
-        // Log each topic name for debugging
-        // topicsData.forEach(topic => {
-        //     logger.debug(`Topic: ${topic.topic_name}`);
-        // });
-        // return topicsData;
+//         // Log each topic name for debugging
+//         // topicsData.forEach(topic => {
+//         //     logger.debug(`Topic: ${topic.topic_name}`);
+//         // });
+//         // return topicsData;
 
-    } catch (error) {
-        // Log the error
-        logger.error('Error fetching topics:', error);
-        throw error; // Re-throw the error to propagate it to the caller
-    }
-}
+//     } catch (error) {
+//         // Log the error
+//         logger.error('Error fetching topics:', error);
+//         throw error; // Re-throw the error to propagate it to the caller
+//     }
+// }
 
-module.exports = fetchTopics;
+// module.exports = fetchTopics;
 

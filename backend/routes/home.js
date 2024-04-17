9ec94e88da22af061cc8ea441d7298162f532d2d
swertheim1 <115668509+swertheim1@users.noreply.github.com>
2024-04-17 01:02:32 -0500

    update home.js

--- backend/routes/home.js
+++ backend/routes/home.js
@@ -7,8 +7,12 @@ const router = express.Router();
 // Define the route handler for the homepage
 router.get('/', (req, res) => {
     // Your logic for rendering the homepage goes here
-    res.send('Welcome to the homepage!');
+    // res.send('Welcome to the homepage!');
+    res.status(201).json({
+        message: 'The homepage is available'
+    });
 });
+
 
 // Export the router to make it available to other parts of the application
 module.exports = router;

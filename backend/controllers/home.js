f48c8f1919a62939e9a43fb4876a31848f543ad1
swertheim1 <115668509+swertheim1@users.noreply.github.com>
2024-04-17 01:31:41 -0500

    updating home.js

--- backend/controllers/home.js
+++ backend/controllers/home.js
@@ -1,9 +1,15 @@
 const pool = require("../pool");
 const jwt = require("jsonwebtoken")
-homeController = [];
+const homeController = {};
 
 homeController.home = async (req, res, next) => {
-    logger.info("Home Controller")
+    try {
+        logger.info("Home Controller");
+        // Your logic for rendering the home page goes here
+        res.send("Welcome to the home page");
+    } catch (error) {
+        next(error); // Pass the error to the error handling middleware
+    }
+};
 
-}
 module.exports = homeController;
--- backend/routes/home.js
+++ backend/routes/home.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const homeController = require('../controllers/home'); 
-require('dotenv').config();
+const homeController = require('../controllers/home');
+const logger = require('../utils/logging/logger'); // Import logger module
 
 // Create an instance of the Express Router
 const homeRouter = express.Router();
 
-homeRouter.route('/home').get(homeController.home);
+// Define the route handler for the home page
+homeRouter.get('/home', homeController.home);
 
 
 // Export the router to make it available to other parts of the application

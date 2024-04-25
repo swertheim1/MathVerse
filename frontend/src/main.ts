011a2163f0865de331be193e0b4755fd6f0a2f9d
swertheim1 <115668509+swertheim1@users.noreply.github.com>
2024-04-24 19:25:18 -0500

    Bug in Signup Service #21

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,9 +4,6 @@ import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 import { enableProdMode } from '@angular/core';
 
-
-
-
 if (environment.production) {
   console.log("Is PRODUCTION: ", environment.production)
   enableProdMode();

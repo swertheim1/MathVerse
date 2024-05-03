259b6f29c440b18dd0912811d78fb0fe73ecd1ef
swertheim1 <115668509+swertheim1@users.noreply.github.com>
2024-05-02 21:09:59 -0500

    submit for production

--- frontend/src/environments/environment.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.environment = void 0;
-exports.environment = {
-    production: false,
-    apiUrl: 'http://localhost:3000'
-};
